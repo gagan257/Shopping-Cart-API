2fdcd70d97c1f694390d4a05289df287f83ac474
Gagan Arora <arorag2577@gmail.com>
2023-02-19 23:55:31 +0530

    Building front page

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+app.use('/CSS',express.static(__dirname+ '/public/CSS'))
+app.use('/Images', express.static(__dirname + '/public/Images'))
 app.use('/', express.static(__dirname + '/public'))
 app.use('/api',require('./routes/api/index').route)
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/public/index.html')
+})
 
 app.listen(2345,()=>{
     console.log("Server started on http://localhost:2345")
